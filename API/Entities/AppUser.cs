6b455551510061d3176b5577caca01b576929801
mfatihkocabas <kocabasmu16@itu.edu.tr>
2021-04-02 13:50:12 +0300

    Adding-Photo-Entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using API.Extentions;
+
 namespace API.Entities
 {
     public class AppUser
@@ -9,6 +13,33 @@ namespace API.Entities
         public byte[] PasswordHash{ get; set; }
 
         public byte[] PasswordSalt{ get; set; }
+
+        public DateTime DateTimeBirth{ get; set; }
+
+        public string KnownAs{ get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set;}
+
+        public ICollection<Photo> Photos{ get; set;}
+
+        public int GetAge()
+        {
+            return DateTimeBirth.CalculateAge();
+        }
+
+
 
         
     }
